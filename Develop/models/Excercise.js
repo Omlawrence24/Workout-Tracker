f59fcf3fe20aec04b3aefce61872457ac573b9cf
Obedia Lawrence <omlawrence24@gmail.com>
2021-04-12 13:51:45 -0400

    model issue

--- Develop/models/Excercise.js
+++ Develop/models/Excercise.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mongo = require("mongo");
 
 const Schema = mongoose.Schema;
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const mongo = require("mongo");
 
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
-const { Excercise, Cardio } = require("./models");
+const Excercise = require("./models");
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
 db.Excercise.create({ type: "Jogging" })
   .then(dbExcercise => {
