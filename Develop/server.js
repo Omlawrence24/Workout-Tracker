6263b62bbc218447e095335d36e466d5b5b0d994
Obedia Lawrence <omlawrence24@gmail.com>
2021-04-14 21:40:02 -0400

    server adjustment

--- Develop/server.js
+++ Develop/server.js
@@ -19,80 +19,80 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
-// Workouts.create({ type: "Jogging" })
-//   .then(dbExcercise => {
-//     console.log(dbExcercise);
-//   })
-//   .catch(({ message }) => {
-//     console.log(message);
-//   });
+Workouts.create({ type: "Jogging" })
+  .then(dbExcercise => {
+    console.log(dbExcercise);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
 
 
-// //   these are the routes that navigate the pages
-// app.get("/",(req, res) => { 
-//     res.sendFile(path.join(__dirname, "./public/index.html"))
-// })
+//   these are the routes that navigate the pages
+app.get("/",(req, res) => { 
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
 
-// app.get("/workout",(req, res) => { 
-//     res.sendFile(path.join(__dirname, "./public/workout.js"))
-// })
+app.get("/workout",(req, res) => { 
+    res.sendFile(path.join(__dirname, "./public/workout.js"))
+})
 
-// app.get("/stats",(req, res) => { 
-//     res.sendFile(path.join(__dirname, "./public/stats.html"))
-// })
-// app.get("/exercise",(req, res) => { 
-//     res.sendFile(path.join(__dirname, "./public/exercise.html"))
-// })
+app.get("/stats",(req, res) => { 
+    res.sendFile(path.join(__dirname, "./public/stats.html"))
+})
+app.get("/exercise",(req, res) => { 
+    res.sendFile(path.join(__dirname, "./public/exercise.html"))
+})
 
 
 
-// // these routes naviagtes the data and how it is recieved or posted 
+// these routes naviagtes the data and how it is recieved or posted 
 
-// // displays the work out dashboard 
-// app.get("/api/stats", (req, res) => {
-//   Workouts.find({})
-//     .then(dbExcercise => {
-//       res.json(dbExcercise);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+// displays the work out dashboard 
+app.get("/api/stats", (req, res) => {
+  Workouts.find({})
+    .then(dbExcercise => {
+      res.json(dbExcercise);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-// // displays all previous workouts 
-// app.get("/api/workouts", (req, res) => {
-//   Workouts.find({})
-//     .then(dbCardio => {
-//       res.json(dbCardio);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+// displays all previous workouts 
+app.get("/api/workouts", (req, res) => {
+  Workouts.find({})
+    .then(dbCardio => {
+      res.json(dbCardio);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-// // creates all added workouts 
-// app.get("/api/exercise", ({ body }, res) => {
-//   Workouts.create(body)
+// creates all added workouts 
+app.get("/api/exercise", ({ body }, res) => {
+  Workouts.create(body)
    
-//     .then(dbExercise => {
-//       res.json(dbExercise);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+    .then(dbExercise => {
+      res.json(dbExercise);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-// // updates one?
-// app.put("/api/exercise", ({ body }, res) => {
-//     Workouts.create(body)
-//       .then(({ _id }) => Workouts.Update({}, { $set: { Workouts: _id } }, { new: true }))
-//       .then(dbExercise => {
-//         res.json(dbExercise);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });
-//   });
+// updates one?
+app.put("/api/exercise/id", ({ body }, res) => {
+    Workouts.create(body)
+      .then(({ _id }) => Workouts.Update({}, { $set: { Workouts: _id } }, { new: true }))
+      .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
   
 
 // Start the server
