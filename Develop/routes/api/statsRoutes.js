0f70d4d21fc5665e3058c98e0e702f32fa7ad1d2
Obedia Lawrence <omlawrence24@gmail.com>
2021-04-17 17:40:00 -0400

    stats

--- Develop/routes/api/statsRoutes.js
+++ Develop/routes/api/statsRoutes.js
@@ -1,27 +1,27 @@
-const router = require('express').Router();
-const { Workouts, Routine } = require('../../models');
+// const router = require('express').Router();
+// const { Workouts, Routine } = require('../../models');
 
-router.post("/stats", (req, res) => {
-    Workouts.create({ type: "Jogging" })
-        .then(dbExcercise => {
-            console.log(dbExcercise);
-        })
-        .catch(({ message }) => {
-            console.log(message);
-        });
-});
+// router.post("/stats", (req, res) => {
+//     Workouts.create({ type: "Jogging" })
+//         .then(dbExcercise => {
+//             console.log(dbExcercise);
+//         })
+//         .catch(({ message }) => {
+//             console.log(message);
+//         });
+// });
 
 
 
-// displays the work out dashboard "MY DASHBOARD"
-router.get("/stats", (req, res) => {
-    Routine.findAll({})
-        .then(dbRoutine => {
-            res.json(dbRoutine);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
+// // displays the work out dashboard "MY DASHBOARD"
+// router.get("/stats", (req, res) => {
+//     Routine.findAll({})
+//         .then(dbRoutine => {
+//             res.json(dbRoutine);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
 
-module.exports = router;
+// module.exports = router;
