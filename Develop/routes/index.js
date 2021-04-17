43ebf55ded9e8d77e5cb232494b1c4116288c089
Obedia Lawrence <omlawrence24@gmail.com>
2021-04-17 17:41:01 -0400

    routesStat

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -9,9 +9,9 @@ router.get("/exercise", (req, res) =>{
 })
 
 
-router.get("/stats", (req, res) =>{
-    res.sendFile(path.join(__dirname, "../public/stats.html"))
-})
+// router.get("/stats", (req, res) =>{
+//     res.sendFile(path.join(__dirname, "../public/stats.html"))
+// })
 
 
 module.exports = router;
